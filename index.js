58d984d7e09f132549edbea157d700e83130f201
Nana Adu-Krow <nadukrow@gmail.com>
2019-04-26 03:23:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+let input = document.querySelector('input')
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function (event) {
+    addNewElementAsLi()
+    resetInput()
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector('ul')
+  let button = document.querySelector('a')
+  button.addEventListener('click', function (event) {
+    ul.innerHTML = ''
+  })
+}
+
+function resetInput(){
+  document.querySelector('input').value = ''
+}
